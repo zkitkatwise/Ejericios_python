1c767a4b4ea1f731273dab2af172b1a3016c1244
zkitkatwise <82173746+zkitkatwise@users.noreply.github.com>
2022-07-14 18:07:17 +0200

    Create 007-calculadora_salario_mensual.py

---
+++ 007-calculadora_salario_mensual.py
@@ -0,0 +1,10 @@
+salarioAnual=input("¿Cuál es tu salario anual?: ")
+if salarioAnual.isdigit():
+    salarioAnual=int(salarioAnual)
+    numMeses=int(input("¿En cuántas pagas quieres calcularlo? (12 o 14): "))
+    salarioMensual=(salarioAnual/numMeses)
+    print("Tu salario mensual es",salarioMensual,"€ en",numMeses,"pagas.")
+elif salarioAnual[0]=="-":
+    print("El valor introducido no es positivo.")
+else:
+    print("El valor que has introducido no es un número.")
