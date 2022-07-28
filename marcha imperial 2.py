41eb9adf46303f2ad80bbac37a50724d9366f86b
zkitkatwise <82173746+zkitkatwise@users.noreply.github.com>
2022-07-28 18:43:37 +0200

    Create marcha imperial 2.py

---
+++ marcha imperial 2.py
@@ -0,0 +1,101 @@
+import winsound
+c=261
+d=294
+e=329
+f=349
+g=391
+gS=415
+a=440
+aS=455
+b=466
+cH=523
+cSH=554
+dH=587
+dSH=622
+eH=659
+fH=698
+fSH=740
+gH=784
+gSH=830
+aH=880
+winsound.Beep(a, 500) 
+winsound.Beep(a, 500)     
+winsound.Beep(a, 500) 
+winsound.Beep(f, 350) 
+winsound.Beep(cH, 150)
+
+winsound.Beep(a, 500)
+winsound.Beep(f, 350)
+winsound.Beep(cH, 150)
+winsound.Beep(a, 1000)
+
+winsound.Beep(eH, 500)
+winsound.Beep(eH, 500)
+winsound.Beep(eH, 500)    
+winsound.Beep(fH, 350) 
+winsound.Beep(cH, 150)
+
+winsound.Beep(gS, 500)
+winsound.Beep(f, 350)
+winsound.Beep(cH, 150)
+winsound.Beep(a, 1000)
+
+winsound.Beep(aH, 500)
+winsound.Beep(a, 350) 
+winsound.Beep(a, 150)
+winsound.Beep(aH, 500)
+winsound.Beep(gSH, 250) 
+winsound.Beep(gH, 250)
+
+winsound.Beep(fSH, 125)
+winsound.Beep(fH, 125)    
+winsound.Beep(fSH, 250)
+
+winsound.Beep(aS, 250)    
+winsound.Beep(dSH, 500)  
+winsound.Beep(dH, 250)  
+winsound.Beep(cSH, 250)  
+
+winsound.Beep(cH, 125)  
+winsound.Beep(b, 125)  
+winsound.Beep(cH, 250)      
+
+winsound.Beep(f, 125)  
+winsound.Beep(gS, 500)  
+winsound.Beep(f, 375)  
+winsound.Beep(a, 125) 
+
+winsound.Beep(cH, 500) 
+winsound.Beep(a, 375)  
+winsound.Beep(cH, 125) 
+winsound.Beep(eH, 1000) 
+
+winsound.Beep(aH, 500)
+winsound.Beep(a, 350) 
+winsound.Beep(a, 150)
+winsound.Beep(aH, 500)
+winsound.Beep(gSH, 250) 
+winsound.Beep(gH, 250)
+
+winsound.Beep(fSH, 125)
+winsound.Beep(fH, 125)    
+winsound.Beep(fSH, 250)
+
+winsound.Beep(aS, 250)    
+winsound.Beep(dSH, 500)  
+winsound.Beep(dH, 250)  
+winsound.Beep(cSH, 250)  
+
+winsound.Beep(cH, 125)  
+winsound.Beep(b, 125)  
+winsound.Beep(cH, 250)      
+
+winsound.Beep(f, 250)  
+winsound.Beep(gS, 500)  
+winsound.Beep(f, 375)  
+winsound.Beep(cH, 125) 
+        
+winsound.Beep(a, 500)            
+winsound.Beep(f, 375)            
+winsound.Beep(c, 125)            
+winsound.Beep(a, 1000)
