44395783b34f5bd878e62b6ee80057a5896fd879
zkitkatwise <82173746+zkitkatwise@users.noreply.github.com>
2022-07-29 21:35:52 +0200

    Update 022-adivinador-planeta.py

--- 022-adivinador-planeta.py
+++ 022-adivinador-planeta.py
@@ -3,12 +3,12 @@ planetas = ["Mercurio","Venus","Marte","Jupiter","Saturno","Urano","Neptuno","Pl
 planeta_secreto=random.choice(planetas)
 numIntentos=1
 print(planetas)
-planeta_candidato=input("Averigua en qué planeta estoy pensando: ").capitalize()
+planeta_candidato=input("Adivina en qué planeta estoy pensando: ").capitalize()
 while planeta_candidato!=planeta_secreto:
     print("--No has acertado--")
     planetas.remove(planeta_candidato)
     numIntentos+=1
     print(planetas)
-    planeta_candidato=input("Averigua en qué planeta estoy pensando: ").capitalize()
+    planeta_candidato=input("Adivina en qué planeta estoy pensando: ").capitalize()
 else:
     print("¡Has acertado! Lo has adivinado en",numIntentos,"intentos.")
