3fcfb424e2cfe34c0427918a2f45faf29683815d
zkitkatwise <82173746+zkitkatwise@users.noreply.github.com>
2022-11-10 03:33:09 +0100

    Nuevo ejercicio (n.29)

---
+++ 029-generador-csv-random/029-generador-csv-random.py
@@ -0,0 +1,13 @@
+import generador_cosas_aleatorias as cosa_aleatoria
+
+NUM_CARACTERES_CONTRASEÑA = 8
+NOMBRE_ARCHIVO = "tabla-contraseñas-nombres-random.csv"
+NUMERO_ELEMENTOS = 100
+
+fichero_csv = open(NOMBRE_ARCHIVO,"w")
+fichero_csv.write("NOMBRE;CONTRASEÑA\n")
+
+for n in range(NUMERO_ELEMENTOS):
+    fichero_csv.write(str(cosa_aleatoria.generar_nombre(1)+";"+cosa_aleatoria.generar_contraseña(NUM_CARACTERES_CONTRASEÑA)+"\n"))
+
+fichero_csv.close
