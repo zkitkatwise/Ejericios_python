7d1f4001d6a4c61b69a5b6175bce78c1b0f10b15
zkitkatwise <82173746+zkitkatwise@users.noreply.github.com>
2022-11-16 00:30:30 +0100

    ej.32(parte2)

--- 032-gestor-cryptomonedas/032-gestor-criptomonedas.py
+++ 032-gestor-cryptomonedas/032-gestor-criptomonedas.py
@@ -1,4 +1,6 @@
 NOMBRE_ARCHIVO = "cotizacion.dat"
+NOMBRE_ARCHIVO_MONEDAS_POSITIVAS = "ventas-positivas.dat"
+NOMBRE_ARCHIVO_MONEDAS_NEGATIVAS = "ventas-negativas.dat"
 
 def obtener_monedas(nombre_archivo):
     monedas = []
@@ -17,9 +19,32 @@ def obtener_monedas_negativas(datos):
         if variacion<0:
             monedas_negativas.append(moneda)
     return monedas_negativas
+def obtener_monedas_positivas(datos):
+    monedas_positivas = []
+    for moneda in datos:
+        variacion = moneda[2]
+        if variacion>0:
+            monedas_positivas.append(moneda)
+    return monedas_positivas
 def nueva_orden_venta(moneda_negativa):
     for valor in moneda_negativa:
         print(f"Orden de venta de la moneda {valor[1]} ({valor[0]}), con una variacion de {valor[2]}")
+def obtener_moneda(datos,nombre_moneda):
+    for moneda in datos:
+        if nombre_moneda==moneda[0]:
+            return moneda[2]
+def escribir_moneda_archivo(nombre_archivo,datos):
+    with open(nombre_archivo,"w") as archivo:
+        for valor in datos:
+            archivo.write(f"Orden de venta de la moneda {valor[1]} ({valor[0]}), con una variacion de {valor[2]}\n")
+
 monedas = obtener_monedas(NOMBRE_ARCHIVO)
 monedas_negativas = obtener_monedas_negativas(monedas)
+escribir_moneda_archivo(NOMBRE_ARCHIVO_MONEDAS_NEGATIVAS,monedas_negativas)
+monedas_positivas = obtener_monedas_positivas(monedas)
+escribir_moneda_archivo(NOMBRE_ARCHIVO_MONEDAS_POSITIVAS,monedas_positivas)
 nueva_orden_venta(monedas_negativas)
+
+moneda_consultada = input("Introduce el nombre de la moneda a buscar: ")
+cotizacion = obtener_moneda(monedas,moneda_consultada)
+print(("La cotizacion de *{0}* es *{1}*").format(moneda_consultada,cotizacion))
